28ba60dd0bd23891bd88877314b055d8a54c8ecf
Naowak <yannis.bendiouis@gmail.com>
2023-03-07 17:34:38 +0100

    fix

--- generate_QA.py
+++ generate_QA.py
@@ -17,7 +17,7 @@ print(f"{args.model} loaded !")
 while True:
 
     # Get input from user
-    question = str(input("\n\nQuestion to ask to the model:\n"))
+    question = str(input("\n\nVotre question pour Overton :\n"))
     if len(question) == 0:
         continue
     
@@ -31,28 +31,30 @@ while True:
 
 
     # Print
-    print("\nText generated:\n")
+    print("\nRéponse :\n")
     
-    MAX_CONTEXT_LENGTH = 1024
-    MAX_GEN_LENGTH = 256
+    MAX_LENGTH = 1024
+    GEN_LENGTH = 64
+    CONTEXT_LENGTH = MAX_LENGTH - GEN_LENGTH
+    
 
     # Generate until <|endoftext|> in sequence
     while not "<|endoftext|>" in text:
 
         # Generate 16 tokens
         ids = tokenizer(text, return_tensors="pt").input_ids.to("cuda")
-        if ids.shape[1] > MAX_CONTEXT_LENGTH :
-            ids = ids[-MAX_CONTEXT_LENGTH:]
+        if ids.shape[1] > CONTEXT_LENGTH :
+            ids = ids[-CONTEXT_LENGTH:]
 
         length = 1 + ids.shape[1]
 
         gen_tokens = model.generate(
             ids,
             temperature=0.7, # The more the temperature is, the more the outputs will be random (0 to 1+)
-            top_p=0.95, # Sum of probability to take into account (the most likelihood words) (0 to 1)
-            top_k=40, # Length of the set of words to pick in (the most likelihood words) (1 to 50+)
-            rep=0.25, # Penalty the model has to generate repetition (0 to 1 : 0 is no penalty)
-            max_length=MAX_CONTEXT_LENGTH + MAX_GEN_LENGTH,
+            top_p=1, # Sum of probability to take into account (the most likelihood words) (0 to 1)
+            top_k=5, # Length of the set of words to pick in (the most likelihood words) (1 to 50+)
+            rep=0.5, # Penalty the model has to generate repetition (0 to 1 : 0 is no penalty)
+            max_length=MAX_LENGTH,
             do_sample=True,
             use_cache=True,
             pad_token_id=tokenizer.eos_token_id
@@ -63,7 +65,7 @@ while True:
         new_text = gen_text[len(text):]
         for i in range(len(new_text)):
             time.sleep(0.001)
-            print(new_text[i], end='')
+            print(new_text[i], end='', flush=True)
 
         # Update text
         text = gen_text
