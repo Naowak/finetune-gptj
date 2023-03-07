1601ca26dbc6782380583c9541a94a17d4c1b493
Naowak <yannis.bendiouis@gmail.com>
2023-03-07 17:50:38 +0100

    u

--- generate_QA.py
+++ generate_QA.py
@@ -34,7 +34,7 @@ while True:
     print("\nRéponse :\n")
     
     MAX_LENGTH = 1024
-    GEN_LENGTH = 64
+    GEN_LENGTH = 8
     CONTEXT_LENGTH = MAX_LENGTH - GEN_LENGTH
     
 
@@ -50,10 +50,10 @@ while True:
 
         gen_tokens = model.generate(
             ids,
-            temperature=0.7, # The more the temperature is, the more the outputs will be random (0 to 1+)
+            temperature=0.9, # The more the temperature is, the more the outputs will be random (0 to 1+)
             top_p=1, # Sum of probability to take into account (the most likelihood words) (0 to 1)
-            top_k=5, # Length of the set of words to pick in (the most likelihood words) (1 to 50+)
-            rep=0.5, # Penalty the model has to generate repetition (0 to 1 : 0 is no penalty)
+            top_k=50, # Length of the set of words to pick in (the most likelihood words) (1 to 50+)
+            rep=0.25, # Penalty the model has to generate repetition (0 to 1 : 0 is no penalty)
             max_length=MAX_LENGTH,
             do_sample=True,
             use_cache=True,
@@ -61,13 +61,10 @@ while True:
         )
 
         # Retrieve generated text and print it
-        gen_text = tokenizer.batch_decode(gen_tokens)[0]
-        new_text = gen_text[len(text):]
-        for i in range(len(new_text)):
-            time.sleep(0.001)
-            print(new_text[i], end='', flush=True)
+        gen_text = tokenizer.batch_decode(gen_tokens[:,-GEN_LENGTH:])[0]
+        print(gen_text, flush=True, end='')
 
         # Update text
-        text = gen_text
+        text += gen_text
 
 
